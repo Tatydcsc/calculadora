a012d6e98bf999f2ba00994b81926ef675896ad1
Tatydcsc <tatydcsc@gmail.com>
2025-01-15 14:47:24 -0300

    exclusão de arqvivos e ajustes

--- src/App.js
+++ src/App.js
@@ -1,26 +1,3 @@
-<<<<<<< HEAD
-import logo from './logo.svg';
-import './App.css';
-
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-=======
 import Input from './components/Input'
 import Button from './components/Button';
 import { Container, Content, Row } from "./styles";
@@ -117,7 +94,6 @@ const App = () => {
         </Row>
       </Content>
     </Container>
->>>>>>> 77a6836 (criação da logica e ajustes no codigo)
   );
 }
 
--- src/index.js
+++ src/index.js
@@ -1,18 +1,3 @@
-<<<<<<< HEAD
-import React from "react";
-import ReactDOM from "react-dom/client";
-import "./index.css";
-import Calculator from "./main/Calculator";
-import reportWebVitals from "./reportWebVitals";
-
-const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(
-  <div>
-    <h1>Calculadora</h1>
-    <Calculator />
-  </div>
-);
-=======
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
@@ -28,4 +13,4 @@ root.render(
   </React.StrictMode>
 );
 
->>>>>>> 77a6836 (criação da logica e ajustes no codigo)
+
